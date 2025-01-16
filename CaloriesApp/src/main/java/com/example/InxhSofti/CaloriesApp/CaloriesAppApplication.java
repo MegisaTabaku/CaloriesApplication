6d4fe44721e16c985cee7c6d7da271dc3f30026d
Megisa Tabaku <tabakumegisa@gmail.com>
2025-01-16 11:40:16 +0100

    shtova nje log in

--- CaloriesApp/src/main/java/com/example/InxhSofti/CaloriesApp/CaloriesAppApplication.java
+++ CaloriesApp/src/main/java/com/example/InxhSofti/CaloriesApp/CaloriesAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CaloriesAppApplication {
-	int kalori;
 	public static void main(String[] args) {
 		SpringApplication.run(CaloriesAppApplication.class, args);
 	}
